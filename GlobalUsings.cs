c66f508e1b3292f70389422aa234c05d50c93b4b
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:19 +0200

    Removed usings outside of projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,14 +1,14 @@
 global using System.Collections.Generic;
 global using System;
-global using SunamoExceptions.InSunamoIsDerivedFrom;
-global using SunamoExceptions.OnlyInSE;
-global using SunamoValues;
+
+
+
 global using System.IO;
 global using System.Threading.Tasks;
-global using SunamoData.Data;
+
 global using FluentFTP;
 global using SunamoFluentFtp.FtpClients;
-global using SunamoFtp.Base;
+
 global using System.Text;
 global using System.Collections.Generic;
 global using System.Collections;
