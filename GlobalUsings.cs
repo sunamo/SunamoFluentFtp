d185021838722b30d31393d892a93c97fe377315
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:08 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -9,3 +9,8 @@ global using SunamoData.Data;
 global using FluentFTP;
 global using SunamoFluentFtp.FtpClients;
 global using SunamoFtp.Base;
+global using System.Text;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.Linq;
+
